fc2cb21fae99372beea2671fa666d4bfa7e320b8
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2019-11-26 10:13:10 +0400

    PW8

---
+++ ex1/app.js
@@ -0,0 +1,11 @@
+$( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true
+    });
+} );
+
+
+
+//ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons ui-accordion-header-collapsed ui-corner-all
+
+//ui-accordion-header ui-corner-top ui-state-default ui-accordion-header-active ui-state-active ui-accordion-icons
